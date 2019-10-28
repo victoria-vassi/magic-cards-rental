b947964e850d7d71f46ec47eaf003687e9b30474
Johannes Weber <mail@johannesweber.me>
2019-10-28 18:02:05 +0800

    Added actions to cards controller and pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    cards = Card.all
+    @cards_selection = cards.sample(8)
   end
 end
