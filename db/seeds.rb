455978b2377619a55288c881b113110587516e89
Pablo Haguet <pablo.haguet@skema.edu>
2019-10-28 16:20:14 +0800

    Added seed.rb with 50 random cards from API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+def generateCards
+ for i in (1..50) do
+    id = rand(1..4880)
+    url = "https://api.magicthegathering.io/v1/cards/#{id}"
+    cards_serialized = open(url).read
+    cards = JSON.parse(cards_serialized)
+
+    cards.each do |element|
+      new_card = Card.create(name: element[1]["name"], color: element[1]["colors"][0], cmc: element[1]["cmc"].to_s, power: element[1]["power"] , toughness: element[1]["toughness"], image_url: element[1]["imageUrl"], price_per_week: rand(5..200))
+      puts new_card
+    end
+    puts " #{i} - Finished generating card whose id is ##{id}."
+  end
+end
+
+generateCards()
