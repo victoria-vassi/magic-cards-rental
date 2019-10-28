076b1d3ee40378ea2d6b46bf54384c962a81ea31
victoria vassileva <victoria.vassileva@yahoo.fr>
2019-10-28 17:38:12 +0800

    Added validation to user, booking and card models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :card
   belongs_to :user
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :total_price, presence: true
 end
