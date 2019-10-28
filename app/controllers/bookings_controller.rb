ff6db2964270c60d9355c11fe8edfc432eccfdfd
Johannes Weber <mail@johannesweber.me>
2019-10-28 18:27:57 +0800

    Added bookings controller (first version)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,36 @@
 class BookingsController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
+  before_action :set_booking, only: [:show]
   def index
+    @bookings = Booking.all
   end
 
   def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+    @card = Card.find(params[:card_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @card = card.find(params[:card_id])
+    @booking.card = @card
+    if @booking.save
+      redirect_to card_path(@card)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :total_price)
   end
 end
