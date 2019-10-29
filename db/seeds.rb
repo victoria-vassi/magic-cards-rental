8716662fc4ad42bfc7dd8532797440bb36ccdb76
Johannes Weber <mail@johannesweber.me>
2019-10-29 17:03:28 +0800

    Refactored seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,89 +3,72 @@ require 'open-uri'
 require 'faker'
 
 # Generating a card, with its associated user(buyer) and booking with its associated user(buyer) and card(whose user_id belongs to the seller). Generating x instances.
-puts "Seeding 16 users, 8 cards and 8 bookings..."
+puts "Seeding 8 buyers (user), 8 sellers (user), 8 cards and 8 bookings..."
+puts "------------------"
 
 for i in (1..8) do
-    id = rand(1..4880)
-    url = "https://api.magicthegathering.io/v1/cards/#{id}"
-    cards_serialized = open(url).read
-    cards = JSON.parse(cards_serialized)
+  id = rand(1..4880)
+  url = "https://api.magicthegathering.io/v1/cards/#{id}"
+  card_serialized = open(url).read
+  card = JSON.parse(card_serialized)
 
-    cards.each do |element|
-
-      new_user_buyer = User.create!(
-        email:    Faker::Internet.email,
-        password: Faker::Internet.password(min_length: 6),
-        first_name:  Faker::Name.first_name,
-        last_name: Faker::Name.last_name,
-      )
-      new_user_seller = User.create!(
-        email:    Faker::Internet.email,
-        password: Faker::Internet.password(min_length: 6),
-        first_name:  Faker::Name.first_name,
-        last_name: Faker::Name.last_name,
-      )
-      new_card = Card.create!(
-        name: element[1]["name"],
-        color: element[1]["colors"][0],
-        card_type: element[1]["type"],
-        cmc: element[1]["cmc"].to_s,
-        power: element[1]["power"] ,
-        toughness: element[1]["toughness"],
-        img_url: element[1]["imageUrl"],
-        price_per_week: p = rand(5..200),
-        user: new_user_seller
-      )
-      new_booking = Booking.create!(
-        start_date:    n = Faker::Date.forward(days: 120),
-        end_date: n+14,
-        total_price: p*2,
-        user: new_user_buyer,
-        card: new_card
-      )
+  new_user_buyer = User.new(
+    email:    Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 6),
+    first_name:  Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+  )
+  new_user_buyer.save
+  if new_user_buyer.errors.empty?
+    puts "--------#{i}---------"
+    puts "Buyer (user) created"
+  else
+    puts new_user_buyer.errors.messages
   end
-puts " #{i} - Created 1 user(seller), 1 card, 1 user(buyer) and 1 booking "
 
+  new_user_seller = User.new(
+    email:    Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 6),
+    first_name:  Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+  )
+  new_user_seller.save
+  if new_user_seller.errors.empty?
+    puts "Seller (user) created"
+  else
+    puts new_user_seller.errors.messages
+  end  
+
+  new_card = Card.new(
+    name: card["card"]["name"],
+    color: card["card"]["colors"][0],
+    card_type: card["card"]["type"],
+    cmc: card["card"]["cmc"].to_s,
+    power: card["card"]["power"] ,
+    toughness: card["card"]["toughness"],
+    img_url: card["card"]["imageUrl"],
+    price_per_week: p = rand(5..200),
+    user: new_user_seller
+  )
+  new_card.save
+  if new_card.errors.empty?
+    puts "Card created"
+  else
+    puts new_card.errors.messages
+  end  
+
+  new_booking = Booking.new(
+    start_date: n = Faker::Date.forward(days: 120),
+    end_date: n+14,
+    total_price: p*2,
+    user: new_user_buyer,
+    card: new_card
+  )
+  new_booking.save
+  if new_booking.errors.empty?
+    puts "Booking created"
+    puts "------------------"
+  else
+    puts new_booking.errors.messages
+  end  
 end
-puts " Finished generating 8 users(sellers) with their 8 associated cards AND 8 users(buyers) with their associated bookings"
-
-
-#Generating a card with its associated user. Seeding it x times.
-# puts "Seeding 8 users(seller) with their 8 associated cards"
-# Card.destroy_all
-# for i in (1..5) do
-#   id = rand(1..4880) #there is 4880 cards in the API we scrape
-#   url = "https://api.magicthegathering.io/v1/cards/#{id}"
-#   cards_serialized = open(url).read
-#   cards = JSON.parse(cards_serialized)
-
-#   cards.each do |element|
-
-#     new_user_seller = User.create!(
-#       email:    Faker::Internet.email,
-#       password: Faker::Internet.password(min_length: 6),
-#       first_name:  Faker::Name.first_name,
-#       last_name: Faker::Name.last_name,
-#     )
-
-#     new_user_buyer = User.create!(
-#       email:    Faker::Internet.email,
-#       password: Faker::Internet.password(min_length: 6),
-#       first_name:  Faker::Name.first_name,
-#       last_name: Faker::Name.last_name,
-#     )
-
-#     new_card = Card.create!(
-#       name: element[1]["name"],
-#       color: element[1]["colors"][0],
-#       card_type: element[1]["type"],
-#       cmc: element[1]["cmc"].to_s,
-#       power: element[1]["power"] ,
-#       toughness: element[1]["toughness"],
-#       img_url: element[1]["imageUrl"],
-#       price_per_week: rand(5..200),
-#       user: new_user_seller
-#     )
-#   end
-#   puts " #{i} - Finished generating a card (##{id} in the API )"
-# end
