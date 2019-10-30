43d50742d0a14b8dc85c4601b0b0e037beb3f395
Johannes Weber <mail@johannesweber.me>
2019-10-30 16:10:23 +0800

    Added typewriter

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,9 @@ import "bootstrap";
 import flatpickr from "flatpickr";
 import { calendar } from '../plugins/init_flatpickr'
 import { refreshSelectorbar } from '../components/selectorbar';
+import { typewriter } from '../components/typewriter';
+
 
 calendar();
 refreshSelectorbar();
-
+typewriter();
