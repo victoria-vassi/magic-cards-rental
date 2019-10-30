5742f9dd0ca1b4752924cfcf614a15adfecece9f
Johannes Weber <mail@johannesweber.me>
2019-10-30 11:46:19 +0800

    Refactored javascript code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,5 @@
 import "bootstrap";
-import { toggleRedBoxShadow } from '../components/selectorbar';
+import { refreshSelectorbar } from '../components/selectorbar';
 import { countSelectedElements } from '../components/selectorbar';
 
-const cards = document.querySelectorAll(".card-trip");
-
-cards.forEach((card) => {
-  card.addEventListener("click", (event) => {
-    countSelectedElements();
-  });
-});
+refreshSelectorbar();
