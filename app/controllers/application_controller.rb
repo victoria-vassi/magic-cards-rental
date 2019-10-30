590541b5ec1f1b0d0e38d2c48401e8af112bbaee
victoria vassileva <victoria.vassileva@yahoo.fr>
2019-10-30 09:30:35 +0800

    Navbar, buttons and footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,8 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
+
+  def after_sign_in_path_for(resource)
+    cards_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
-  resources :cards, only: [:new, :create, :index, :show] do 
+  resources :cards, only: [:new, :create, :index, :show] do
     resources :bookings, only: [:new, :create, :index, :show]
-  end 
-
+  end
 end
