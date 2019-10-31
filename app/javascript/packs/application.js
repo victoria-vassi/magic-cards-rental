68400766a798455cfde08d20aca4201158454c5a
Johannes Weber <mail@johannesweber.me>
2019-10-31 11:59:58 +0800

    Added filter form to cards page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,12 @@
 import "bootstrap";
 import flatpickr from "flatpickr";
 import { calendar } from '../plugins/init_flatpickr'
+import { filter } from '../plugins/init_flatpickr'
 import { refreshSelectorbar } from '../components/selectorbar';
 import { openCity } from '../components/tabbar';
 import { typewriter } from '../components/typewriter';
 
 calendar();
+filter();
 refreshSelectorbar();
 openCity();
